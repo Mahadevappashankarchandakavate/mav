e109de89517a009812498b399e6f6acebbb09feb
Rathandeep2004 <chandakavatemahadevappa@gmail.com>
2024-12-28 12:01:04 +0530

    Initial commit

---
+++ 3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include<ctype.h>
+
+bool verifyRegistration(char registration[]) {
+    if (strlen(registration) != 10) {
+        return false;
+    }
+
+    for (int i = 0; i < 2; i++) {
+        if (!isalpha(registration[i])) {
+            return false;
+        }
+    }
+
+    for (int i = 2; i < 4; i++) {
+        if (!isdigit(registration[i])) {
+            return false;
+        }
+    }
+
+    for (int i = 4; i < 6; i++) {
+        if (!isalpha(registration[i])) {
+            return false;
+        }
+    }
+
+    for (int i = 6; i < 10; i++) {
+        if (!isdigit(registration[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    char registration[11];
+    printf("Enter a vehicle registration number: ");
+    scanf("%s", registration);
+
+    if (verifyRegistration(registration)) {
+        printf("Accept: Vehicle registration number is valid.\n");
+    } else {
+        printf("Reject: Vehicle registration number is invalid.\n");
+    }
+
+    return 0;
+}
